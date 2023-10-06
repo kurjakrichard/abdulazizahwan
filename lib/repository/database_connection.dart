f5ade001fd9b1b4b191e54d1313fe7431594f7c9
Kurják Richárd <kurjak.richard@gmail.com>
2023-10-06 19:44:17 +0200

    remove unnecessiary dependency

--- lib/repository/database_connection.dart
+++ lib/repository/database_connection.dart
@@ -1,6 +1,5 @@
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:sqflite/sqflite.dart';
 // ignore: depend_on_referenced_packages
 import 'package:path/path.dart';
 import 'dart:io';
